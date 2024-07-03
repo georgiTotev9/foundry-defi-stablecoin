506def326d227cb2c068cdd146978c6def739d1c
Georgi Dimov Totev <110054543+georgiTotev9@users.noreply.github.com>
2024-07-03 13:32:39 +0300

    initial commit

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+/*
+ * @title DecentralizedStableCoin
+ * @author Georgi Totev
+ * Collateral: Exogenous
+ * Minting (Stability Mechanism): Decentralized (Algorithmic)
+ * Value (Relative Stability): Anchored (Pegged to USD)
+ * Collateral Type: Crypto
+ *
+* This is the contract meant to be owned by DSCEngine. It is a ERC20 token that can be minted and burned by the
+DSCEngine smart contract.
+ */
+
+contract DecentralizeStableCoin {
+    constructor() {
+
+    }
+}
