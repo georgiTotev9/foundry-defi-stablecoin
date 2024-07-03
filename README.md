3840f62daf7625ebe97dd6b399a978fdbd1f538f
Georgi Dimov Totev <110054543+georgiTotev9@users.noreply.github.com>
2024-07-03 13:48:41 +0300

    updated coin

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 1. (Relative Stability) Anchored or Pegged -> $1.00
-    1. Chainling Price feed.
+    1. Chainlink Price feed.
     2. Set a function to exchange ETH & BTC -> $$$
 2. Stability Mechanism (Minting): Algorithmic (Decentralized)
     1.People can only mint the stablecoin with enough collateral (coded)
--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -3,6 +3,8 @@
 pragma solidity ^0.8.18;
 
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
 /*
  * @title DecentralizedStableCoin
  * @author Georgi Totev
@@ -15,8 +17,38 @@ import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensio
 DSCEngine smart contract.
  */
 
-contract DecentralizeStableCoin {
-    constructor() {
+contract DecentralizeStableCoin is ERC20Burnable, Ownable {
 
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizeStableCoin__NotZeroAddress();
+
+    constructor()
+        ERC20("DecentralizeStableCoin", "DSC")
+        Ownable(address(this))
+    {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        if (balance < _amount){
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns(bool){
+        if(_to == address(0)){
+            revert DecentralizeStableCoin__NotZeroAddress();
+        }
+
+        if(_to <= address(0)){
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
     }
 }
